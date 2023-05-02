2bc4203a6ad5dcb5ed25ac56bb5890ad4bde3a67
Lanqing Yuan <yuanlq@uchicago.edu>
2023-05-02 17:48:03 -0500

    version bump

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open('HISTORY.md') as file:
 
 setup(
     name="utilix",
-    version="0.7.2",
+    version="0.7.3",
     url='https://github.com/XENONnT/utilix',
     description="User-friendly interface to various utilities for XENON users",
     long_description_content_type='text/markdown',
