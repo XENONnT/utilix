c5d95963600f23dde753e6278f1ef5383072bd23
Lanqing Yuan <yuanlq@uchicago.edu>
2023-05-11 00:11:56 -0500

    added xenon_service_proxy

--- utilix/batchq.py
+++ utilix/batchq.py
@@ -42,7 +42,10 @@ TMPDIR = {
 def overwrite_dali_bind(bind, partition):
     """Check if we are binding non-dali storage when we are on dali compute node. If yes, then overwrite"""
     if partition == 'dali':
-        bind = ['/dali', '/dali/lgrandi/xenonnt/xenon.config:/project2/lgrandi/xenonnt/xenon.config']
+        bind = ['/dali', 
+                '/dali/lgrandi/xenonnt/xenon.config:/project2/lgrandi/xenonnt/xenon.config', 
+                '/dali/lgrandi/grid_proxy/xenon_service_proxy:/project2/lgrandi/grid_proxy/xenon_service_proxy'
+                ]
         print("You are using dali parition, and your bind has been fixed to %s"%(bind))
     return bind
 
