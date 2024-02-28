766cae2a73f60c477959a2ec80e781fae6ae36fd
Yibo Wei <david_wyb2001@outlook.com>
2024-02-27 16:14:22 -0800

    Completely refactor batchq with simple_slurm and pydantic

--- utilix/batchq.py
+++ utilix/batchq.py
@@ -1,265 +1,261 @@
-import subprocess
+import datetime
 import os
+import subprocess
+import re
 import tempfile
-import shlex
+from typing import Literal
+from pydantic import BaseModel, Field, validator
+from simple_slurm import Slurm
 from utilix import logger
-import getpass
 
-sbatch_template = """#!/bin/bash
-
-#SBATCH --job-name={jobname}
-#SBATCH --output={log}
-#SBATCH --error={log}
-#SBATCH --account=pi-lgrandi
-#SBATCH --qos={qos}
-#SBATCH --partition={partition}
-#SBATCH --mem-per-cpu={mem_per_cpu}
-#SBATCH --cpus-per-task={cpus_per_task}
-{node}
-{exclude_nodes}
-{hours}
-
-{job}
-"""
-
-SINGULARITY_DIR = {
-    'dali': '/dali/lgrandi/xenonnt/singularity-images',
-    'lgrandi': '/project2/lgrandi/xenonnt/singularity-images',
-    'xenon1t': '/project2/lgrandi/xenonnt/singularity-images',
-    'broadwl': '/project2/lgrandi/xenonnt/singularity-images',
-    'kicp': '/project2/lgrandi/xenonnt/singularity-images',
-    'caslake': '/project2/lgrandi/xenonnt/singularity-images',
-}
-
-SCRATCH_DIR = os.environ.get('SCRATCH')
-if len(SCRATCH_DIR.split('/'))==3 and SCRATCH_DIR.split('/')[-1]=='midway3':
-    SCRATCH_DIR += '/%s'%(os.environ['USER'])
-
+USER = os.environ.get("USER")
+SCRATCH_DIR = os.environ.get("SCRATCH", ".")
+PARTITIONS = ["dali", "lgrandi", "xenon1t", "broadwl", "kicp", "caslake"]
 TMPDIR = {
-    'dali': os.path.expanduser('/dali/lgrandi/%s/tmp'%(getpass.getuser())),
-    'lgrandi': os.path.join(SCRATCH_DIR, 'tmp'),
-    'xenon1t': os.path.join(os.environ.get('SCRATCH', '.'), 'tmp'),
-    'broadwl': os.path.join(os.environ.get('SCRATCH', '.'), 'tmp'),
-    'kicp': os.path.join(os.environ.get('SCRATCH', '.'), 'tmp'),
-    'caslake': os.path.join(SCRATCH_DIR, 'tmp'),
+    "dali": os.path.expanduser(f"/dali/lgrandi/{USER}/tmp"),
+    "lgrandi": os.path.join(SCRATCH_DIR, "tmp"),
+    "xenon1t": os.path.join(SCRATCH_DIR, "tmp"),
+    "broadwl": os.path.join(SCRATCH_DIR, "tmp"),
+    "kicp": os.path.join(SCRATCH_DIR, "tmp"),
+    "caslake": os.path.join(SCRATCH_DIR, "tmp"),
 }
+SINGULARITY_DIR = {
+    "dali": "/dali/lgrandi/xenonnt/singularity-images",
+    "lgrandi": "/project2/lgrandi/xenonnt/singularity-images",
+    "xenon1t": "/project2/lgrandi/xenonnt/singularity-images",
+    "broadwl": "/project2/lgrandi/xenonnt/singularity-images",
+    "kicp": "/project2/lgrandi/xenonnt/singularity-images",
+    "caslake": "/project2/lgrandi/xenonnt/singularity-images",
+}
+DEFAULT_BIND = [
+    "/project2/lgrandi/xenonnt/dali:/dali",
+    "/project2",
+    "/project",
+    "/scratch/midway2/%s" % (USER),
+    "/scratch/midway3/%s" % (USER),
+]
+DALI_BIND = [
+    "/dali/lgrandi",
+    "/dali/lgrandi/xenonnt/xenon.config:/project2/lgrandi/xenonnt/xenon.config",
+    "/dali/lgrandi/grid_proxy/xenon_service_proxy:/project2/lgrandi/grid_proxy/xenon_service_proxy",
+]
 
 
-def overwrite_dali_bind(bind, partition):
-    """Check if we are binding non-dali storage when we are on dali compute node. If yes, then overwrite"""
-    if partition == 'dali':
-        bind = ['/dali/lgrandi', 
-                '/dali/lgrandi/xenonnt/xenon.config:/project2/lgrandi/xenonnt/xenon.config', 
-                '/dali/lgrandi/grid_proxy/xenon_service_proxy:/project2/lgrandi/grid_proxy/xenon_service_proxy'
-                ]
-        # print("You are using dali parition, and your bind has been fixed to %s"%(bind))
-    return bind
+def _make_executable(path: str) -> None:
+    """
+    Make a file executable by the user, group and others.
 
+    Args:
+        path (str): Path to the file to make executable.
 
-def wrong_log_dir(path):
-    """Check if the directory is NOT in dali"""
-    abs_path = os.path.abspath(path)
-    top_level = abs_path.split('/')[1] 
-    wrong_log_dir = False   
-    if top_level != 'dali':
-        wrong_log_dir = True
-    else:
-        wrong_log_dir = False
-    return wrong_log_dir
-
-
-def overwrite_dali_job_log(path, partition):
-    if partition == 'dali':
-        if wrong_log_dir(path):
-            logname = os.path.abspath(path).split('/')[-1]
-            new_path = TMPDIR['dali']+'/'+logname
-            print('Your log is relocated at: %s' % (new_path))
-            return new_path
-    print('Your log is located at: %s' % (os.path.abspath(path)))
-    return path
-
-
-def make_executable(path):
-    """Make the file at path executable, see """
+    Raises:
+        FileNotFoundError: If the file does not exist.
+    """
+    if not os.path.exists(path):
+        raise FileNotFoundError(f"File {path} does not exist")
     mode = os.stat(path).st_mode
-    mode |= (mode & 0o444) >> 2    # copy R bits to X
+    mode |= (mode & 0o444) >> 2
     os.chmod(path, mode)
 
 
-def singularity_wrap(jobstring, image, bind, partition):
-    """Wraps a jobscript into another executable file that can be passed to singularity exec"""
-    file_descriptor, exec_file = tempfile.mkstemp(suffix='.sh', dir=TMPDIR[partition])
-    make_executable(exec_file)
-    os.write(file_descriptor, bytes('#!/bin/bash\n' + jobstring, 'utf-8'))
-    bind_string = " ".join([f"--bind {b}" for b in bind])
-    image = os.path.join(SINGULARITY_DIR[partition], image)
-    new_job_string = f"""singularity exec {bind_string} {image} {exec_file}
-exit_code=$?
-rm {exec_file}
-if [ $exit_code -ne 0 ]; then
-    echo "Python script failed with exit code $exit_code"
-    exit $exit_code
-fi
-"""
-    os.close(file_descriptor)
-    return new_job_string
+class JobSubmission(BaseModel):
+    jobstring: str = Field(..., description="The command to execute")
+    log: str = Field("job.log", description="Where to store the log file of the job")
+    partition: Literal["dali", "lgrandi", "xenon1t", "broadwl", "kicp", "caslake"] = Field(
+        "xenon1t", description="Partition to submit the job to"
+    )
+    qos: str = Field("xenon1t", description="QOS to submit the job to")
+    account: str = Field("pi-lgrandi", description="Account to submit the job to")
+    jobname: str = Field("somejob", description="How to name this job")
+    dry_run: bool = Field(
+        False, description="Only print how the job looks like, without submitting"
+    )
+    mem_per_cpu: int = Field(1000, description="MB requested for job")
+    container: str = Field(
+        "xenonnt-development.simg", description="Name of the container to activate"
+    )
+    bind: list[str] = Field(
+        default_factory=lambda: DEFAULT_BIND,
+        description="Paths to add to the container. Immutable when specifying dali as partition",
+    )
+    cpus_per_task: int = Field(1, description="CPUs requested for job")
+    hours: float = Field(None, description="Max hours of a job")
+    node: str = Field(None, description="Define a certain node to submit your job")
+    exclude_nodes: str = Field(
+        None, description="Define a list of nodes which should be excluded from submission"
+    )
+    dependency: str = Field(
+        None, description="Provide list of job ids to wait for before running this job"
+    )
 
+    @validator("bind", pre=True, each_item=True)
+    def check_bind(cls, v):
+        if not isinstance(v, str):
+            raise ValueError("Each bind must be a string")
 
+        if not os.path.exists(v):
+            raise FileNotFoundError(f"Bind path {v} does not exist")
 
-def submit_job(jobstring,
-               log='job.log',
-               partition='xenon1t',
-               qos='xenon1t',
-               account='pi-lgrandi',
-               jobname='somejob',
-               sbatch_file=None,
-               dry_run=False,
-               mem_per_cpu=1000,
-               container='xenonnt-development.simg',
-               bind=['/project2/lgrandi/xenonnt/dali:/dali', '/project2', '/project', '/scratch/midway2/%s'%(getpass.getuser()), '/scratch/midway3/%s'%(getpass.getuser())],
-               cpus_per_task=1,
-               hours=None,
-               node=None,
-               exclude_nodes=None,
-               dependency=None,
-               **kwargs
-               ):
-    """
-    Submit a job to the dali/midway batch queue
+        return v
 
-    EXAMPLE
-        from utilix import batchq
-        import time
+    @validator("partition", pre=True, always=True)
+    def check_partition(cls, v):
+        if v not in PARTITIONS:
+            raise ValueError(f"Partition must be one of {PARTITIONS}")
+        return v
 
-        job_log = 'job.log'
-        batchq.submit_job('echo "say hi"', log=job_log)
+    @validator("partition", pre=True, always=True)
+    def overwrite_for_dali(cls, v, values):
+        # You can access other fields in the model using the "values" dict
+        if v == "dali":
+            bind = DALI_BIND
+            values["bind"] = bind
+            logger.warning(f"Binds are overwritten to {bind}")
+            # If log path top level is not /dali
+            abs_log_path = os.path.abspath(values["log"])
+            if not abs_log_path.startswith("/dali"):
+                log_filename = os.path.basename(abs_log_path)
+                new_log_path = f"{TMPDIR['dali']}/{log_filename}"
+                values["log"] = new_log_path
+                print("Your log is relocated at: %s" % (new_log_path))
+                logger.warning(f"Log path is overwritten to {new_log_path}")
+        return v
 
-        time.sleep(10) # Allow the job to run
-        for line in open(job_log):
-            print(line)
+    @validator("hours")
+    def check_hours_value(cls, v):
+        if v is not None and (v <= 0 or v > 72):
+            raise ValueError("Hours must be between 0 and 72")
+        return v
 
-    :param jobstring: the command to execute
-    :param log: where to store the log file of the job
-    :param partition: partition to submit the job to
-    :param qos: qos to submit the job to
-    :param account: account to submit the job to
-    :param jobname: how to name this job
-    :param sbatch_file: where to write the job script to
-    :param dry_run: only print how the job looks like
-    :param mem_per_cpu: mb requested for job
-    :param container: name of the container to activate
-    :param bind: which paths to add to the container. This is immutable when you specified dali as partition
-    :param cpus_per_task: cpus requested for job
-    :param hours: max hours of a job
-    :param node: define a certain node to submit your job should be submitted to
-    :param exclude_nodes: define a list of nodes which should be excluded from submission
-    :param dependency: provide list of job ids to wait for before running this job
-    :param kwargs: are ignored
-    :return: None
-    """
-    if 'delete_file' in kwargs:
-        logger.warning('"delete_file" option for "submit_job" has been removed, ignoring for now')
-    os.makedirs(TMPDIR[partition], exist_ok=True)
-    # overwrite bind to make sure dali is isolated
-    bind = overwrite_dali_bind(bind, partition)
+    @validator("node", "exclude_nodes", "dependency")
+    def check_node_format(cls, v):
+        if v is not None and not re.match(r"^[a-zA-Z0-9,\[\]-]+$", v):
+            raise ValueError("Invalid format for node/exclude_nodes/dependency")
+        return v
 
-    # overwrite log directory if it is not on dali and you are running on dali.
-    log = overwrite_dali_job_log(log, partition)
+    @validator("container")
+    def check_container_format(cls, v):
+        if not v.endswith(".simg"):
+            raise ValueError("Container must end with .simg")
 
-    if container:
-        # need to wrap job into another executable
-        jobstring = singularity_wrap(jobstring, container, bind, partition)
+    def __init__(self, **data):
+        super().__init__(**data)
+        os.makedirs(TMPDIR[self.partition], exist_ok=True)
 
-        # if you have exported INSTALL_CUTAX to be 1, then you do NOT need to unset CUTAX_LOCATION
-        print("Your current CUTAX_LOCATION is %s"%(os.environ.get('CUTAX_LOCATION')))
-        if os.environ.get('INSTALL_CUTAX') != '1':
-            print("You have NOT exported INSTALL_CUTAX to be 1, so I will not unset CUTAX_LOCATION.")
-            jobstring = 'unset X509_CERT_DIR\n' + 'module load singularity\n' + jobstring
+    def __singularity_wrap(self) -> str:
+        """
+        Wrap the jobstring with the singularity command.
+
+        Raises:
+            FileNotFoundError: If the singularity image does not exist.
+
+        Returns:
+            str: The new jobstring with the singularity command.
+        """
+        if self.dry_run:
+            file_discriptor = None
+            exec_file = f"{TMPDIR[self.partition]}/tmp.sh"
         else:
-            print("You have exported INSTALL_CUTAX to be 1, so I will unset CUTAX_LOCATION for you.")
-            jobstring = 'unset X509_CERT_DIR CUTAX_LOCATION\n' + 'module load singularity\n' + jobstring
+            file_discriptor, exec_file = tempfile.mkstemp(suffix=".sh", dir=TMPDIR[self.partition])
+            _make_executable(exec_file)
+            os.write(file_discriptor, bytes("#!/bin/bash\n" + self.jobstring, "utf-8"))
+        bind_string = " ".join([f"--bind {b}" for b in self.bind])
+        image = os.path.join(SINGULARITY_DIR[self.partition], self.container)
+        if not os.path.exists(image):
+            raise FileNotFoundError(f"Singularity image {image} does not exist")
+        new_job_string = (
+            f"unset X509_CERT_DIR CUTAX_LOCATION\n"
+            f"module load singularity\n"
+            f"singularity exec {bind_string} {image} {exec_file}\n"
+            f"exit_code=$?\n"
+            f"rm {exec_file}\n"
+            f"if [ $exit_code -ne 0 ]; then\n"
+            f"    echo Python script failed with exit code $exit_code\n"
+            f"    exit $exit_code\n"
+            f"fi\n"
+        )
+        if file_discriptor is not None:
+            os.close(file_discriptor)
+        return new_job_string
 
-    if not hours is None:
-        hours = '#SBATCH --time={:02d}:{:02d}:{:02d}'.format(int(hours), int(hours * 60 % 60), int(hours * 60 % 60 * 60 % 60))
-    else:
-        hours = ''
+    def submit(self):
+        """
+        Submit the job to the SLURM queue.
+        """
+        # Initialize a dictionary with mandatory parameters
+        slurm_kwargs = {
+            "job_name": self.jobname,
+            "output": self.log,
+            "error": self.log,
+            "account": self.account,
+            "qos": self.qos,
+            "partition": self.partition,
+            "mem_per_cpu": self.mem_per_cpu,
+            "cpus_per_task": self.cpus_per_task,
+        }
 
-    if not node is None:
-        if not isinstance(node, str):
-            raise ValueError(f'node should be str but given {type(node)}')
-        node = '#SBATCH --nodelist={node}'.format(node=node)
-    else:
-        node = ''
+        # Conditionally add optional parameters if they are not None
+        if self.hours is not None:
+            slurm_kwargs["time"] = datetime.timedelta(hours=self.hours)
+        if self.node is not None:
+            slurm_kwargs["nodelist"] = self.node
+        if self.exclude_nodes is not None:
+            slurm_kwargs["exclude"] = self.exclude_nodes
+        if self.dependency is not None:
+            slurm_kwargs["dependency"] = {"afterok": self.dependency}
+            slurm_kwargs["kill_on_invalid"] = "yes"
 
-    if not exclude_nodes is None:
-        if not isinstance(exclude_nodes, str):
-            raise ValueError(f'exclude_nodes should be str but given {type(exclude_nodes)}')
-            # string like 'myCluster01,myCluster02,myCluster03' or 'myCluster[01-09]'
-        exclude_nodes = '#SBATCH --exclude={exclude_nodes}'.format(exclude_nodes=exclude_nodes)
-    else:
-        exclude_nodes = ''
+        # Create the Slurm instance with the conditional arguments
+        slurm = Slurm(**slurm_kwargs)
 
-    if not dependency is None:
-        if isinstance(dependency, list):
-            # Handle list of strings
-            job_ids = ":".join(dependency)
-        elif isinstance(dependency, str):
-            # Handle single string
-            job_ids = dependency
-        else:
-            raise ValueError(f'dependency should be list or str but given {type(dependency)}')
-        
-        dependency = "--dependency=afterok:"+job_ids+" --kill-on-invalid-dep=yes"
-    
-    else:
-        dependency = ''
+        # Process the jobstring with the container if specified
+        if self.container is not None:
+            self.jobstring = self.__singularity_wrap(
+                self.jobstring, self.container, self.bind, self.partition
+            )
 
+        # Add the job command
+        slurm.add_cmd(self.jobstring)
+
+        print(f"Your log is located at: {self.log}")
+
+        # Handle dry run scenario
+        if self.dry_run:
+            print(slurm)
+            return
+
+        # Submit the job
+        slurm.sbatch()
 
 
-    sbatch_script = sbatch_template.format(jobname=jobname, log=log, qos=qos, partition=partition,
-                                           account=account, job=jobstring, mem_per_cpu=mem_per_cpu,
-                                           cpus_per_task=cpus_per_task, hours=hours, node=node,
-                                           exclude_nodes=exclude_nodes)
+def submit_jobs(**kwargs):
+    """
+    Adapter to old function name.
+    You should use JobSubmission to get modern code editor support.
 
-    if dry_run:
-        print("=== DRY RUN ===")
-        print(sbatch_script)
-        return
+    Args:
+        **kwargs: Keyword arguments to pass to JobSubmission
+    """
+    logger.warning(
+        "Using legacy function name, please use JobSubmission to get modern code editor support"
+    )
+    job = JobSubmission(**kwargs)
+    job.submit()
 
-    if sbatch_file is None:
-        remove_file = True
-        _, sbatch_file = tempfile.mkstemp(suffix='.sbatch')
-    else:
-        remove_file = False
 
-    with open(sbatch_file, 'w') as f:
-        f.write(sbatch_script)
+def count_jobs() -> int:
+    """
+    Count the number of jobs in the queue.
 
-    command = "sbatch %s %s" % (dependency, sbatch_file)
-    if not sbatch_file:
-        print("Executing: %s" % command)
-
-    process = subprocess.Popen(shlex.split(command), stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
-    output, error = process.communicate()
- 
-
-    # Check if the job submission was successful
-    if process.returncode == 0:
-        # Extract the job ID from the output
-        job_id = output.decode().strip().split()[-1]
-    else:
-        job_id = None
-        # Handle the case where the job submission failed
-        print("Job submission failed:", error.decode())
-        
-    if remove_file:
-        os.remove(sbatch_file)
-
-    return job_id
-
-def count_jobs(string=''):
-    username = os.environ.get("USER")
-    output = subprocess.check_output(shlex.split("squeue -u %s" % username))
-    lines = output.decode('utf-8').split('\n')
-    return len([job for job in lines if string in job])
-
+    Returns:
+        int: The number of jobs in the queue.
+    """
+    cmd = f"squeue -u {USER}"
+    try:
+        result = subprocess.run(cmd, capture_output=True, text=True, check=True, shell=True)
+        lines = result.stdout.strip().split("\n")
+        # Subtract 1 for the header; if no jobs, ensure it returns 0 instead of -1
+        return max(len(lines) - 1, 0)
+    except subprocess.CalledProcessError as e:
+        print(f"An error occurred while executing squeue: {e}")
+        return 0
