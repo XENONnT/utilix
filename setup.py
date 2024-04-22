6bebcf8f32907367fb367ebef0056c0af3c90319
Yue Ma <3124558229@qq.com>
2024-04-22 12:08:49 -0700

    bump version -> 0.8.3

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open('HISTORY.md') as file:
 
 setup(
     name="utilix",
-    version="0.8.0",
+    version="0.8.3",
     url='https://github.com/XENONnT/utilix',
     description="User-friendly interface to various utilities for XENON users",
     long_description_content_type='text/markdown',
